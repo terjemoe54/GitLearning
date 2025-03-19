c9ccc1ff5e4f558503207777c38a0a94c37c2747
Terje Moe <terjemoe54@hotmail.com>
2025-03-19 09:05:51 +0100

    Initial Commit

---
+++ GitLearning/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GitLearning
+//
+//  Created by Terje Moe on 19/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Terje Moe on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
